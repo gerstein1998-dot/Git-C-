54ac21aac5e70d6679a3a1d331ce6079fbb9ff23
gerstein1998-dot <gerstein1998@gmail.com>
2026-06-17 12:18:21 +0300

    עדכון

---
+++ Exceptions/EXCEPTIONS 1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EXCEPTIONS_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numKM, numLiter;
+            double res = 0;
+            // קליטה של מספר עשרוני וזריקת חריגה מתאימה 
+            try
+            {
+                Console.WriteLine("please enter num of km:");
+                numKM = double.Parse(Console.ReadLine());
+                
+                if (numKM > 999 || numKM <= 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+               
+                Console.WriteLine("please enter num of liter:");
+                numLiter = double.Parse(Console.ReadLine());
+              
+                    res = numKM / numLiter;
+                Console.WriteLine($"mileage is {res} miles per gallon. ");
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("no car can drive that many miles.");  
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("input string was not in a correct format." + fe);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+
+         
+        }
+    }
+}
---
+++ Exceptions/exception 2/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace exception_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = 10000;
+            string id = "";
+            string cardtNum = "";
+            int amount = 0;
+            bool isSucccess = false;
+            while (!isSucccess)
+            {
+                try
+                {
+                    Console.WriteLine("please enter your id number: ");
+                    id = Console.ReadLine();
+                    if (id.Length < 9 || cardtNum.Length < 16)
+                    {
+                        throw new OverflowException(@"the credit \ id number  you enterd is too short.");
+                    }
+                    Console.WriteLine("please enter your card number: ");
+                    cardtNum = Console.ReadLine();
+                    if (id.Length > 9 ||)
+                    {
+                        throw new OverflowException();
+                    }
+                    bool isIdLenghtValid = (id.Length == 9);
+                    bool isCardLenghtValid = (cardtNum.Length == 16);
+                    if (isIdLenghtValid != isCardLenghtValid)
+                    {
+                        throw new FormatException();
+                    }
+
+                    Console.WriteLine("please enter the amount you need: ");
+                    amount = int.Parse(Console.ReadLine());
+                    if (amount > balance)
+                    {
+                        throw new FormatException();
+                    }
+
+                    isSucccess = true;
+                }
+
+                catch (OverflowException)
+                {
+                   
+                    else
+                    {
+                        Console.WriteLine(@"the credit \ id number is too long.");
+                    }
+                    Console.WriteLine("press any key to try again..");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                catch (FormatException)
+                {
+                    if (amount > balance)
+                    {
+                        Console.WriteLine("there is not enough cash in the balance.");
+                    }
+                    Console.WriteLine("press any key to try again..");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+        }
+    }
+}
